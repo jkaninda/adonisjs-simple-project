78a275470d1c1947bb868362106dbd0fa833d54a
Jonas Kaninda <jonaskaninda@gmail.com>
2022-08-11 11:37:11 +0200

    r

--- .history/app/Controllers/Http/PostsController_20220811113604.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import View from '@ioc:Adonis/Core/View'
-
-export default class PostsController {
-    public async index ({ }: HttpContextContract) {
-        return View.render('posts.index')
-    }
-}
-
