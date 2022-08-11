0ed11ae6c145a4563355662974fa3274aac66694
Jonas Kaninda <jonaskaninda@gmail.com>
2022-08-11 11:36:29 +0200

    r

--- .history/app/Controllers/Http/PostsController_20220701011940.ts
+++
@@ -1,8 +0,0 @@
- import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class PostsController {
-    public async index ({ request, response }: HttpContextContract) {
-        return response.json({ message: 'Hello World' })
-    }
-}
-
--- .history/app/Controllers/Http/PostsController_20220701012540.ts
+++
@@ -1,9 +0,0 @@
- import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import View from '@ioc:Adonis/Core/View'
-
-export default class PostsController {
-    public async index ({ request, response }: HttpContextContract) {
-        return View.render('posts.index')
-    }
-}
-
